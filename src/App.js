89c2e406dbaf0d14b90d355f3ec2c1b845ddf797
Samantha Tran <stran@Samanthas-MacBook-Air.local>
2022-08-23 00:33:18 +1000

    added json website data + iniital ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { useState } from 'react';
+import {ChatContainer, 
+        MessageList, 
+        Message, 
+        MessageInput,
+        ConversationHeader,
+        Avatar,
+        VoiceCallButton,
+        VideoCallButton,
+        InfoButton,
+        MessageGroup,
+        MessageSeparator,
+         } from '@chatscope/chat-ui-kit-react';
+import MessageData from './data/MessageData';
+import avatar from './assets/profile-photo.jpeg'
 
 function App() {
+
+  const [message, setMessage] = useState("introduction")
+  const [optionData, setOptions] = useState(MessageData["options"])
+  const [messageHistory, setMessageHistory] = useState(MessageData["introduction"]["message"])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        height: "500px"
+      }}>
+        <ChatContainer>
+          <ConversationHeader>
+            <Avatar src={avatar} name="Samantha Tran" status="available"/>
+            <ConversationHeader.Content userName="Samantha Tran" info="Active Now" />
+            <ConversationHeader.Actions>
+              <VoiceCallButton />
+              <VideoCallButton />
+              <InfoButton />
+            </ConversationHeader.Actions>          
+          </ConversationHeader>
+            <MessageList>
+            <MessageSeparator content="Saturday, 30 November 2019" />
+            <MessageGroup direction="incoming" sender="Lilly" sentTime="just now" avatarPosition="tl">
+              <Avatar src={avatar} name="Lilly" />      
+              <MessageGroup.Messages>
+                <Message model={{
+                  message: "Hello my friend"
+                }} />
+                <Message model={{
+                  message: "Hello my friend"
+                }} />
+                <Message model={{
+                  message: "Hello my friend"
+                }} />
+                <Message model={{
+                  message: "Hello my friend"
+                }} />
+              </MessageGroup.Messages>
+            </MessageGroup>
+            <Message model={{
+              message: "Hello my friend",
+              direction: "outgoing",
+              position: "single"
+            }} />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />        
+        </ChatContainer>
+      </div>
     </div>
   );
 }
---
+++ src/data/MessageData.js
@@ -0,0 +1,61 @@
+const MessageData = {
+    "introduction": {
+        "message": ["Hi there! My name is Samantha. It is nice to meet you"]
+    },
+    "about": {
+        "message": [
+            "I am a software engineering student at The University of Queensland. I am due to graduate in Summer 2023\
+            with a bachelor in Software Engineering (Honours).",
+            "In my spare time, I enjoy baking, graphic design and meeting up with my friends!",
+            "I am currently looking for opportunities to strengthen my skills, connections and \
+            gain hands on industry experience."
+        ],
+    },
+    "skills": [
+        "The programming languges I know are: Python, Java, HTML/CSS/Javascript, Haskell and SQL",
+        "The developer tools I am familiar with are: Git, Sonarcloud, JaCoCo, Adobe Illustrator, Adobe Photoshop",
+        "The frameworks I am familiar with are: React, Vue, Flask, Tailwindcss, CodeIgniter",
+        "I consider myself a fast learner and am open to learning new things on the fly!"
+    ],
+    "work": [
+        "I am currently a casual academic tutor at the University of Queensland. The course I tutor teaches students\
+        about working in large teams, with a focus on version control tools, build tools, testing / mocking, design patterns\
+        and communication tools (UML diagrams, Kanban boards, etc)",
+        "I have also worked as a Software Engineer Intern at Products For Industry and Swyftx. For more information,\
+        I recommend taking a look at my resume or LinkedIn!"
+    ],
+    "projects": [
+        "Here are some of the projects I have worked on:",
+        "Pinterest Board Collager - Program to scrape images from a Pinterest board and output an optimised \
+        collage of said users.",
+        "Online Therapy Site - Mock website/forum for parents with children experiencing mental health issues \
+        to book virtual appointments with specialists, and to seek advice from other parents.",
+        "LearnToSew - All-in-one sewing platform where instructors can easily monetise their sewing \
+        courses and students can learn / buy patterns.",
+        "For more information, I recommend taking a look at my resume, LinkedIn or Github!"
+    ],
+    "contact": {
+        "message": [
+            "I'd love to have a chat! You can contact me through:",
+            "email: samantha.tran@uq.net.au",
+            "linkedin: linkedin/samanthatran2"
+        ]
+    },
+    "links": {
+        "message": [
+            "sdfs",
+            "sfsdf",
+            "sdfdsf"
+        ]
+    },
+    "options": [
+        {"about": "Tell me about yourself"},
+        {"skills": "What are your skills"},
+        {"work": "Can I see your work experience?"},
+        {"projects": "What projects have you worked on?"},
+        {"contact": "How can I contact you?"},
+        {"links": "Can I see your Resume, Github or LinkedIn?"}
+    ]
+}
+
+export default MessageData
